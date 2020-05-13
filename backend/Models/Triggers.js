81dc751239d499686dab32b01a298c5bc3e58014
Siddhant Shingi <snshingi19@gmail.com>
2020-05-14 02:40:20 +0530

    Backend: Triggers Added to Update tokensVerified

--- backend/Models/Triggers.js
+++ backend/Models/Triggers.js
@@ -1,11 +1,11 @@
 let mysqlConfig = require("../Utilities/mysqlConfig");
 
 let initialize = () => {
-	mysqlConfig.getDB().query("DROP TRIGGER IF EXISTS updateCapacityLeftOnInsert");
-	mysqlConfig.getDB().query("CREATE TRIGGER updateCapacityLeftOnInsert AFTER INSERT ON tokens FOR EACH ROW BEGIN IF NEW.status = 1 THEN SET @cap = (SELECT capacityLeft FROM shopBookings WHERE shopId = NEW.shopId AND date = NEW.date AND startTime = NEW.startTime AND duration = NEW.duration LIMIT 1); UPDATE shopBookings SET capacityLeft = @cap - 1 WHERE shopId = NEW.shopId AND date = NEW.date AND startTime = NEW.startTime AND duration = NEW.duration; END IF;END");
+	mysqlConfig.getDB().query("DROP TRIGGER IF EXISTS updateShopBookingsOnInsert");
+	mysqlConfig.getDB().query("CREATE TRIGGER updateShopBookingOnInsert AFTER INSERT ON tokens FOR EACH ROW BEGIN IF NEW.status = 1 THEN UPDATE shopBookings SET capacityLeft = capacityLeft - 1 WHERE shopId = NEW.shopId AND date = NEW.date AND startTime = NEW.startTime AND duration = NEW.duration; END IF; IF NEW.verified = 1 THEN UPDATE shopBookings SET tokensVerified = tokensVerified + 1 WHERE shopId = NEW.shopId AND date = NEW.date AND startTime = NEW.startTime AND duration = NEW.duration; END IF; END");
 	console.log("trigger 1 created if not EXISTS");
-	mysqlConfig.getDB().query("DROP TRIGGER IF EXISTS updateCapacityLeftOnUpdate");
-	mysqlConfig.getDB().query("CREATE TRIGGER updateCapacityLeftOnUpdate AFTER UPDATE ON tokens FOR EACH ROW BEGIN IF NEW.status = 4 AND OLD.status = 1 THEN UPDATE shopBookings SET capacityLeft = capacityLeft + 1 WHERE shopId = NEW.shopId AND date = NEW.date AND startTime = NEW.startTime AND duration = NEW.duration; END IF; IF NEW.status = 1 AND OLD.status = 2 THEN UPDATE shopBookings SET capacityLeft = GREATEST(capacityLeft - 1, 0) WHERE shopId = NEW.shopId AND date = NEW.date AND startTime = NEW.startTime AND duration = NEW.duration; END IF; END");
+	mysqlConfig.getDB().query("DROP TRIGGER IF EXISTS updateShopBookingsOnUpdate");
+	mysqlConfig.getDB().query("CREATE TRIGGER updateShopBookingsOnUpdate AFTER UPDATE ON tokens FOR EACH ROW BEGIN IF NEW.status = 4 AND OLD.status = 1 THEN UPDATE shopBookings SET capacityLeft = capacityLeft + 1 WHERE shopId = NEW.shopId AND date = NEW.date AND startTime = NEW.startTime AND duration = NEW.duration; END IF; IF NEW.status = 1 AND OLD.status = 2 THEN UPDATE shopBookings SET capacityLeft = GREATEST(capacityLeft - 1, 0) WHERE shopId = NEW.shopId AND date = NEW.date AND startTime = NEW.startTime AND duration = NEW.duration; END IF; IF NEW.verified = 1 AND OLD.verified = 0 THEN UPDATE shopBookings SET tokensVerified = tokensVerified + 1 WHERE shopId = NEW.shopId AND date = NEW.date AND startTime = NEW.startTime AND duration = NEW.duration;END IF; END");
 	console.log("trigger 2 created if not EXISTS");
 	
 }
@@ -15,19 +15,23 @@ module.exports = {
 }
 
 // trigger 1
-// CREATE TRIGGER updateCapacityLeftOnInsert
+// CREATE TRIGGER updateShopBookingOnInsert
 // AFTER INSERT 
 // ON tokens FOR EACH ROW 
 // BEGIN 
 // 	IF NEW.status = 1 THEN
-// 		SET @cap = (SELECT capacityLeft FROM shopBookings WHERE shopId = NEW.shopId AND date = NEW.date AND startTime = NEW.startTime AND duration = NEW.duration LIMIT 1); 
-// 		UPDATE shopBookings SET capacityLeft = @cap - 1 WHERE shopId = NEW.shopId AND date = NEW.date AND startTime = NEW.startTime AND duration = NEW.duration; 
+// 		UPDATE shopBookings SET capacityLeft = capacityLeft - 1 WHERE shopId = NEW.shopId AND date = NEW.date AND startTime = NEW.startTime AND duration = NEW.duration; 
 // 	END IF;
+
+// 	IF NEW.verified = 1 THEN
+//         UPDATE shopBookings SET tokensVerified = tokensVerified + 1 WHERE shopId = NEW.shopId AND date = NEW.date AND startTime = NEW.startTime AND duration = NEW.duration;
+//     END IF;
 // END
 
+// SET @cap = (SELECT capacityLeft FROM shopBookings WHERE shopId = NEW.shopId AND date = NEW.date AND startTime = NEW.startTime AND duration = NEW.duration LIMIT 1); 
 
 // trigger 2
-// CREATE TRIGGER updateCapacityLeftOnUpdate
+// CREATE TRIGGER updateShopBookingsOnUpdate
 // AFTER UPDATE
 // ON tokens FOR EACH ROW
 // BEGIN
@@ -37,5 +41,9 @@ module.exports = {
 
 //     IF NEW.status = 1 AND OLD.status = 2 THEN 
 //         UPDATE shopBookings SET capacityLeft = GREATEST(capacityLeft - 1, 0) WHERE shopId = NEW.shopId AND date = NEW.date AND startTime = NEW.startTime AND duration = NEW.duration;
+//     END IF;
+
+//     IF NEW.verified = 1 AND OLD.verified = 0 THEN
+//         UPDATE shopBookings SET tokensVerified = tokensVerified + 1 WHERE shopId = NEW.shopId AND date = NEW.date AND startTime = NEW.startTime AND duration = NEW.duration;
 //     END IF;
 // END
