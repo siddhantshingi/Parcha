d389a5b1f30c062c82dc45d792afda093cc344e6
Akshat Khare <akshat.khare08@gmail.com>
2020-05-15 15:27:02 +0530

    module coded

--- backend/temp2.js
+++ backend/temp2.js
@@ -7,7 +7,7 @@ var encryptStringWithRsaPublicKey = function(toEncrypt, publicKey) {
     // var absolutePath = path.resolve(relativeOrAbsolutePathToPublicKey);
     // var publicKey = fs.readFileSync(absolutePath, "utf8");
     // console.log(publicKey.toString())
-    var buffer = new Buffer(toEncrypt);
+    var buffer = new Buffer.from(toEncrypt);
     var encrypted = crypto.publicEncrypt(publicKey, buffer);
     return encrypted.toString("base64");
 };
@@ -15,7 +15,7 @@ var encryptStringWithRsaPrivateKey = function(toEncrypt, privateKey) {
     // var absolutePath = path.resolve(relativeOrAbsolutePathToPublicKey);
     // var publicKey = fs.readFileSync(absolutePath, "utf8");
     // console.log(privateKey.toString())
-    var buffer = new Buffer(toEncrypt);
+    var buffer = new Buffer.from(toEncrypt);
     // var encrypted = crypto.privateEncrypt(privateKey, buffer);
     var encrypted = crypto.privateEncrypt({
             key: privateKey.toString(),
@@ -28,7 +28,7 @@ var encryptStringWithRsaPrivateKey = function(toEncrypt, privateKey) {
 var decryptStringWithRsaPrivateKey = function(toDecrypt, privateKey) {
     // var absolutePath = path.resolve(relativeOrAbsolutePathtoPrivateKey);
     // var privateKey = fs.readFileSync(absolutePath, "utf8");
-    var buffer = new Buffer(toDecrypt, "base64");
+    var buffer = new Buffer.from(toDecrypt, "base64");
     //var decrypted = crypto.privateDecrypt(privateKey, buffer);
     const decrypted = crypto.privateDecrypt(
         {
@@ -43,7 +43,7 @@ var decryptStringWithRsaPublicKey = function(toDecrypt, publicKey) {
     // var absolutePath = path.resolve(relativeOrAbsolutePathtoPrivateKey);
     // var privateKey = fs.readFileSync(absolutePath, "utf8");
     // console.log(publicKey)
-    var buffer = new Buffer(toDecrypt, "base64");
+    var buffer = new Buffer.from(toDecrypt, "base64");
     //var decrypted = crypto.privateDecrypt(privateKey, buffer);
     const decrypted = crypto.publicDecrypt(
         {
@@ -81,33 +81,75 @@ function generateKeys(modulusLength) {
     return [publicKey, privateKey]
 }
 
-values = generateKeys(1024);
+values = generateKeys(2048);
 shopPublicKey = values[0];
 shopPrivateKey = values[1];
-values = generateKeys(2048);
+values = generateKeys(4096);
 cusPublicKey = values[0];
 cusPrivateKey = values[1];
 
 // let a = encryptStringWithRsaPublicKey("hello", "public.pem", publicKey)
 // let b = decryptStringWithRsaPrivateKey(a, "private.pem", privateKey);
-var token = "token";
+var token = "12-14,20/5/2020,akshat,34,dilipstore";
 var tokenencryptedSPUB = encryptStringWithRsaPublicKey(token, shopPublicKey);
 // console.log("tokenencryptedSPUB is")
 // console.log(tokenencryptedSPUB)
 var tokenencryptedCPRI_SPUB = encryptStringWithRsaPrivateKey(tokenencryptedSPUB, cusPrivateKey);
 console.log("tokenencryptedCPRI_SPUB is");
 console.log(tokenencryptedCPRI_SPUB);
-console.log("customerPublicKey is")
-console.log(cusPublicKey)
+console.log("customerPublicKey is");
+console.log(cusPublicKey);
+console.log("message total size is ")
+console.log(tokenencryptedCPRI_SPUB.length+cusPublicKey.length)
+console.log(tokenencryptedCPRI_SPUB.length+"+"+cusPublicKey.length)
 var tokendecryptedCPUB = decryptStringWithRsaPublicKey(tokenencryptedCPRI_SPUB, cusPublicKey);
 console.log("tokendecryptedCPUB is");
 console.log(tokendecryptedCPUB);
-console.log("shopKeeperPrivateKey is")
 var tokendecryptedCPUB_SPRI = decryptStringWithRsaPrivateKey(tokendecryptedCPUB, shopPrivateKey);
 console.log(tokendecryptedCPUB_SPRI);
 
+values = generateKeys(4096);
+shopPublicKey = values[0];
+shopPrivateKey = values[1];
+values = generateKeys(2048);
+cusPublicKey = values[0];
+cusPrivateKey = values[1];
+
+var token = "12-14,20/5/2020,akshat,34,dilipstore";
+var tokenencryptedCPRI = encryptStringWithRsaPrivateKey(token, cusPrivateKey);
+
+var tokenencryptedSPUB_CPRI = encryptStringWithRsaPublicKey(tokenencryptedCPRI, shopPublicKey);
+// console.log("tokenencryptedSPUB is")
+// console.log(tokenencryptedSPUB)
+console.log("tokenencryptedSPUB_CPRI is");
+console.log(tokenencryptedSPUB_CPRI);
+console.log("customerPublicKey is");
+console.log(cusPublicKey);
+console.log("message total size is ")
+console.log(tokenencryptedSPUB_CPRI.length+cusPublicKey.length)
+console.log(tokenencryptedSPUB_CPRI.length+"+"+cusPublicKey.length)
+var tokendecryptedSPRI = decryptStringWithRsaPrivateKey(tokenencryptedSPUB_CPRI, shopPrivateKey);
+console.log("tokendecryptedSPRI is");
+console.log(tokendecryptedSPRI);
+var tokendecryptedSPRI_CPUB = decryptStringWithRsaPublicKey(tokendecryptedSPRI, cusPublicKey);
+console.log(tokendecryptedSPRI_CPUB);
 // var a = encryptStringWithRsaPrivateKey("hello", privateKey);
 // console.log(a);
 // console.log(typeof a);
 // var b = decryptStringWithRsaPublicKey(a, publicKey);
 // console.log(b)
+values = generateKeys(4096);
+serverPublicKey = values[0];
+serverPrivateKey = values[1];
+var token = "12-14,20/5/2020,akshat,34,dilipstore";
+var tokenencryptedSERPRI = encryptStringWithRsaPrivateKey(token, serverPrivateKey);
+
+// console.log("tokenencryptedSPUB is")
+// console.log(tokenencryptedSPUB)
+console.log("tokenencryptedSERPRI is");
+console.log(tokenencryptedSERPRI);
+console.log("message total size is ")
+console.log(tokenencryptedSERPRI.length)
+var tokendecryptedSERPUB = decryptStringWithRsaPublicKey(tokenencryptedSERPRI, serverPublicKey);
+console.log("tokendecryptedSERPUB is");
+console.log(tokendecryptedSERPUB);
