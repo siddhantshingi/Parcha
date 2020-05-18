106a32693cff3d637be553d24a7d67ed0c43fe97
srijan-sinha <srijansinha159@gmail.com>
2020-05-19 04:58:47 +0530

    API calls and Entities updated

--- token_system/lib/Entities/authority.dart
+++ token_system/lib/Entities/authority.dart
@@ -2,54 +2,56 @@ import 'package:token_system/Entities/abstract.dart';
 
 class Authority extends Entity {
   int id;
-  String email;
-  String contactNumber;
   String name;
-  String aadharNumber;
-  String password;
+  String email;
+  String mobileNumber;
+  String aadhaarNumber;
   String state;
   String district;
   String pincode;
-  int verificationStatus;
+  int authVerification;
 
   Authority({
     this.id,
-    this.email,
-    this.contactNumber,
     this.name,
-    this.aadharNumber,
-    this.password,
+    this.email,
+    this.mobileNumber,
+    this.aadhaarNumber,
     this.state,
     this.district,
     this.pincode,
-    this.verificationStatus,
+    this.authVerification,
   });
 
-  Map<String, dynamic> toJson() =>
-      {
-        "id": this.id.toString(),
+  Map<String, dynamic> registerToJson(String password) => {
         "name": this.name,
         "email": this.email,
-        "contactNumber": this.contactNumber,
-        "aadharNumber": this.aadharNumber,
-        "password": this.password,
-        "state": this.state,
-        "district": this.district,
+        "password": password,
+        "mobileNumber": this.mobileNumber,
+        "aadharNumber": this.aadhaarNumber,
         "pincode": this.pincode,
-        "verificationStatus": this.verificationStatus.toString(),
       };
-  factory Authority.fromJson(Map<String, dynamic> json) {
+
+  Map<String, dynamic> updateToJson(
+          {String name, String mobileNumber, String aadhaarNumber, String pincode}) =>
+      {
+        "id": this.id.toString(),
+        "name": name ?? this.name,
+        "mobileNumber": mobileNumber ?? this.mobileNumber,
+        "aadharNumber": aadhaarNumber ?? this.aadhaarNumber,
+        "pincode": pincode ?? this.pincode,
+      };
+
+  factory Authority.verifyFromJson(Map<String, dynamic> json, String email) {
     return Authority(
         id: json['id'] as int,
         name: json['name'] as String,
-        email: json['email'] as String,
-        contactNumber: json['contactNumber'] as String,
-        aadharNumber: json['aadharNumber'] as String,
-        password: json['password'] as String,
+        email: email,
+        mobileNumber: json['mobileNumber'] as String,
+        aadhaarNumber: json['aadhaarNumber'] as String,
         state: json['state'] as String,
         district: json['district'] as String,
         pincode: json['pincode'] as String,
-        verificationStatus: json['verificationStatus'] as int
-    );
+        authVerification: json['authVerification'] as int);
   }
 }
