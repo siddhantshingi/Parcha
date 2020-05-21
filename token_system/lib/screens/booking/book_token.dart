11f3059448b43d98c39c86a32a321c80e727bed4
DivyanshuSaxena <dssaxena2011@gmail.com>
2020-05-21 14:41:37 +0530

    Added token booking confirmation for users

--- token_system/lib/screens/booking/book_token.dart
+++ token_system/lib/screens/booking/book_token.dart
@@ -12,14 +12,13 @@ import 'package:token_system/Services/miscServices.dart';
 import 'package:token_system/Services/tokenService.dart';
 
 class BookScreen extends StatelessWidget {
+  final Shop shop;
   final User user;
   final GlobalKey<TabNavigatorState> tn;
-  final Shop shop;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   BookScreen({Key key, @required this.user, @required this.tn, @required this.shop})
       : super(key: key);
-
-  void alertMessage(String status) {}
 
   @override
   Widget build(BuildContext context) {
@@ -34,109 +33,114 @@ class BookScreen extends StatelessWidget {
       return shopBookings;
     };
 
-    return Column(children: <Widget>[
-      SectionTitle(heading: 'Confirm Token'),
-      Text(
-        shop.shopName,
-        style: TextStyle(color: Colors.amber[800], fontSize: 24),
-      ),
-      Expanded(
+    return Scaffold(
+      key: _scaffoldKey,
+      body: Column(children: <Widget>[
+        SectionTitle(heading: 'Confirm Token'),
+        Text(
+          shop.shopName,
+          style: TextStyle(color: Colors.amber[800], fontSize: 24),
+        ),
+        Expanded(
           child: AsyncBuilder(
-        future: MiscService.getShopBookingsApi(shop),
-        builder: (bookings) {
-          return ListView.builder(
-            shrinkWrap: true,
-            itemCount: bookings.length,
-            itemBuilder: (BuildContext context, int index) {
-              return ListTile(
-                leading: Icon(Icons.confirmation_number),
-                title: Text(slotNumStartTime(bookings[index].slotNumber) + "  " + bookings[index].date.toString()),
-                subtitle: Text('Capacity \u{2192}   Left: ' +
-                    bookings[index].capacityLeft.toString() +
-                    '   Max: ' +
-                    bookings[index].maxCapacity.toString()),
-                trailing: FlatButton.icon(
-                  color: Color.fromARGB(255, 255, 69, 0),
-                  onPressed: () {
-                    return showDialog<void>(
-                      context: context,
-                      barrierDismissible: false, // user must tap button!
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: Text('Confirm your booking at ' + shop.shopName),
-                          content: SingleChildScrollView(
-                            child: ListBody(
-                              children: <Widget>[
-                                Text(
-                                  'Time: ' +
-                                      slotNumStartTime(bookings[index].slotNumber) +
-                                      ' - ' +
-                                      slotNumEndTime(bookings[index].slotNumber),
-                                  style: TextStyle(fontSize: 16),
-                                ),
-                                Text('Would you like to confirm this booking?'),
-                              ],
-                            ),
-                          ),
-                          actions: <Widget>[
-                            FlatButton(
-                              child: Text('Confirm'),
-                              onPressed: () {
-                                var status = TokenService.bookTokenApi(Token(
-                                        shopId: shop.id,
-                                        shopName: shop.shopName,
-                                        userId: user.id,
-                                        userEmail: user.email,
-                                        userName: user.name,
-                                        date: DateTime.now().toString().substring(0, 11),
-                                        slotNumber: bookings[index].slotNumber))
-                                    .then((code) {
-                                  //TODO: Need a proper UI message.
-                                  if (code == 200)
-                                    return 'Confirmed';
-                                  else if (code == 201)
-                                    return 'Waitlisted';
-                                  else if (code == 409)
-                                    return 'Token already exists';
-                                  else if (code == 404)
-                                    return 'Slot does not exists';
-                                  else
-                                    return 'Server Error';
-                                });
-                                Navigator.of(context).pop();
-                                AlertDialog(
-                                  title: Text(status),
-                                  actions: <Widget>[
-                                    FlatButton(
-                                      child: Text('Close'),
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
+            future: MiscService.getShopBookingsApi(shop),
+            builder: (bookings) {
+              return ListView.builder(
+                shrinkWrap: true,
+                itemCount: bookings.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    leading: Icon(Icons.confirmation_number),
+                    title: Text(slotNumStartTime(bookings[index].slotNumber) +
+                        "  " +
+                        bookings[index].date.toString()),
+                    subtitle: Text('Capacity \u{2192}   Left: ' +
+                        bookings[index].capacityLeft.toString() +
+                        '   Max: ' +
+                        bookings[index].maxCapacity.toString()),
+                    trailing: FlatButton.icon(
+                      color: Color.fromARGB(255, 255, 69, 0),
+                      onPressed: () async {
+                        await showDialog<String>(
+                          context: _scaffoldKey.currentContext,
+                          barrierDismissible: false, // user must tap button!
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text('Confirm your booking at ' + shop.shopName),
+                              content: SingleChildScrollView(
+                                child: ListBody(
+                                  children: <Widget>[
+                                    Text(
+                                      'Time: ' +
+                                          slotNumStartTime(bookings[index].slotNumber) +
+                                          ' - ' +
+                                          slotNumEndTime(bookings[index].slotNumber),
+                                      style: TextStyle(fontSize: 16),
                                     ),
+                                    Text('Would you like to confirm this booking?'),
                                   ],
-                                );
-                              },
-                            ),
-                            FlatButton(
-                              child: Text('Cancel'),
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                            ),
-                          ],
+                                ),
+                              ),
+                              actions: <Widget>[
+                                FlatButton(
+                                  child: Text('Confirm'),
+                                  onPressed: () async {
+                                    final status = await TokenService.bookTokenApi(Token(
+                                            shopId: shop.id,
+                                            shopName: shop.shopName,
+                                            userId: user.id,
+                                            userEmail: user.email,
+                                            userName: user.name,
+                                            date: DateTime.now().toString().substring(0, 11),
+                                            slotNumber: bookings[index].slotNumber))
+                                        .then((code) {
+                                      //TODO: Need a proper UI message.
+                                      if (code == 200)
+                                        return 'Confirmed';
+                                      else if (code == 201)
+                                        return 'Waitlisted';
+                                      else if (code == 409)
+                                        return 'Token already exists';
+                                      else if (code == 404)
+                                        return 'Slot does not exist';
+                                      else
+                                        return 'Server Error';
+                                    });
+
+                                    if (status != null) {
+                                      print(status);
+                                      final snackbar = SnackBar(
+                                        content: Text('Status of booking: ' +
+                                            status +
+                                            '. Please check User History for further actions.'),
+                                      );
+                                      _scaffoldKey.currentState.showSnackBar(snackbar);
+                                    }
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                                FlatButton(
+                                  child: Text('Cancel'),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                              ],
+                            );
+                          },
                         );
                       },
-                    );
-                  },
-                  icon: Icon(Icons.add_circle),
-                  label: Text('Book'),
-                ),
+                      icon: Icon(Icons.add_circle),
+                      label: Text('Book'),
+                    ),
+                  );
+                },
               );
             },
-          );
-        },
-        onReceiveJson: onReceiveJson,
-      ))
-    ]);
+            onReceiveJson: onReceiveJson,
+          ),
+        )
+      ]),
+    );
   }
 }
