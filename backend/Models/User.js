44901886ee349447826d699f6c3d63651c8494c6
Siddhant Shingi <snshingi19@gmail.com>
2020-05-28 15:43:00 +0530

    Minor bugs fixed

--- backend/Models/User.js
+++ backend/Models/User.js
@@ -1,7 +1,7 @@
 let mysqlConfig = require("../Utilities/mysqlConfig");
 
 let initialize = () => {
-	mysqlConfig.getDB().query("create table IF NOT EXISTS users (id INT auto_increment primary key, name VARCHAR(255), email VARCHAR(255), password VARCHAR(255), mobileNumber VARCHAR(10), aadharNumber VARCHAR(12), state VARCHAR(255), district VARCHAR(255), pincode VARCHAR(6), emailVerification INT, mobileVerification INT)");
+	mysqlConfig.getDB().query("create table IF NOT EXISTS users (id INT auto_increment primary key, name VARCHAR(255), email VARCHAR(255), password VARCHAR(255), mobileNumber VARCHAR(10), aadhaarNumber VARCHAR(12), state VARCHAR(255), district VARCHAR(255), pincode VARCHAR(6), emailVerification INT, mobileVerification INT)");
 	console.log("Users table created if not EXISTS");
 }
 
